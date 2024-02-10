3e57c783c1784028c3c172c8fe31c139a97325b0
Hana Spoustová <bernreih@gmail.com>
2024-02-11 00:04:15 +0100

    reseni

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const goodbye = (name) => {
+  return 'S pozdravem ' + name;
+};
+
+const fillSubject = (subject) => {
+  document.querySelector('.email__subject').textContent = subject;
+};
+
+const fillBody = (body, name, goodBye) => {
+  const bodyElm = document.querySelector('.email__body');
+  bodyElm.innerHTML += body;
+  const closingElm = document.querySelector('.email__closing');
+  closingElm.textContent = goodBye(name);
+};
+
+fillSubject('Nepřišly gumičky');
+fillBody(
+  'Nepřišly gumičky. A co na to počítač? Mlčí. No tak to je konec. Všecko vylejt.',
+  'referent Kubrt',
+  rudeGoodBye
+
+);
+
+
+function rudeGoodBye (name) {
+
+return "Sbohem a šáteček" + name
+
+}
+
+function niceGoodBye (name) {
+
+  return "S přáním pěkného dne" + name
+  
+  }
+
+
+
 
